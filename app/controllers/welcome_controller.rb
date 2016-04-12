f353cfdb6d233ee80f8fffeca233eb1c8bc7c5f2
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-04-12 18:51:41 +0000

    add styling to user profile page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   
   def index
-    
+    if user_signed_in?
+      redirect_to users_show_url
+    end
   end
 end
 #write skip before_action
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  def avatar_url(size)
+     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+     "https://s-media-cache-ak0.pinimg.com/favicons/fe2172ad86a337a478ad3ad852faf809887f550cb63f7ff1bb2de5f9.png?4a1dbe3038214acc47b3bd3dddec2425
+/#{gravatar_id}.png?s=#{size}"
+  end
 
 #////////Do I need the below if I am using Devise?
  # config.email_regexp = /\A[^@]+@[^@]+\z/
